21f645ddbf95d1f88721aeacb8b14944c9e4d0dc
fura2 <yamazoe0577@gmail.com>
2026-05-06 14:16:33 +0900

    add definitions for monoid and monoid action

---
+++ algebra/monoid.hpp
@@ -0,0 +1,18 @@
+#include <concepts>
+
+template <typename M>
+concept Monoid =
+    requires { typename M::value_type; } &&
+    requires(const typename M::value_type& a, const typename M::value_type& b) {
+      { M::op(a, b) } -> std::same_as<typename M::value_type>;
+      { M::id() } -> std::same_as<typename M::value_type>;
+    };
+
+// template <typename T>
+// struct AddMonoid {
+//   using value_type = T;
+//   static value_type op(const value_type& a, const value_type& b) {
+//     return a + b;
+//   }
+//   static value_type id() { return {}; }
+// };
---
+++ algebra/monoid_action.hpp
@@ -0,0 +1,16 @@
+#include <concepts>
+
+#include "algebra/monoid.hpp"
+
+template <typename A>
+concept MonoidAction =
+    requires {
+      typename A::value_monoid;
+      typename A::action_monoid;
+    } && Monoid<typename A::value_monoid> &&
+    Monoid<typename A::action_monoid> &&
+    requires(const typename A::value_monoid::value_type& x,
+             const typename A::action_monoid::value_type& f) {
+      // right action
+      { A::act(x, f) } -> std::same_as<typename A::value_monoid::value_type>;
+    };
