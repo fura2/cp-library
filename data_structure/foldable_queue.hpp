f629ff4e5bb8c7a659be1cc13567d316c2ae5ac4
fura2 <yamazoe0577@gmail.com>
2026-05-07 21:05:27 +0900

    fix many bugs for FoldableQueue

--- data_structure/foldable_queue.hpp
+++ data_structure/foldable_queue.hpp
@@ -16,33 +16,23 @@ class FoldableQueue {
   using size_type = std::vector<value_type>::size_type;
 
   FoldableQueue(): cum_front{M::id()}, cum_back{M::id()} {}
-  FoldableQueue(const std::vector<value_type>& v)
-      : stk_back{v},
+  explicit FoldableQueue(const std::vector<value_type>& v)
+      : stk_back(v),
         cum_front{M::id()},
-        cum_back{std::ranges::fold_left(v, M::id(), M::op)} {}
-  FoldableQueue(std::vector<value_type>&& v)
-      : stk_back{v},
+        cum_back{std::ranges::fold_left(stk_back, M::id(), M::op)} {}
+  explicit FoldableQueue(std::vector<value_type>&& v)
+      : stk_back(std::move(v)),
         cum_front{M::id()},
-        cum_back{std::ranges::fold_left(v, M::id(), M::op)} {}
+        cum_back{std::ranges::fold_left(stk_back, M::id(), M::op)} {}
 
   bool empty() const { return stk_front.empty() && stk_back.empty(); }
   size_type size() const { return stk_front.size() + stk_back.size(); }
 
-  value_type& front() {
-    return stk_front.empty() ? stk_back.front() : stk_front.back();
-  }
   const value_type& front() const {
     return stk_front.empty() ? stk_back.front() : stk_front.back();
   }
-  value_type& back() {
-    return stk_back.empty() ? stk_front.front() : stk_back.back();
-  }
   const value_type& back() const {
     return stk_back.empty() ? stk_front.front() : stk_back.back();
-  }
-  value_type& operator[](size_type i) {
-    return i < stk_front.size() ? stk_front[stk_front.size() - i - 1]
-                                : stk_back[i - stk_front.size()];
   }
   const value_type& operator[](size_type i) const {
     return i < stk_front.size() ? stk_front[stk_front.size() - i - 1]
@@ -50,27 +40,32 @@ class FoldableQueue {
   }
 
   void push(const value_type& x) {
-    stk_back.emplace_back(x);
-    cum_back = M::op(cum_back, x);
+    const value_type& y = stk_back.emplace_back(x);
+    cum_back = M::op(cum_back, y);
   }
   void push(value_type&& x) {
-    stk_back.emplace_back(x);
-    cum_back = M::op(cum_back, x);
+    const value_type& y = stk_back.emplace_back(std::move(x));
+    cum_back = M::op(cum_back, y);
   }
   template <typename R>
     requires std::ranges::input_range<R> &&
              std::convertible_to<std::ranges::range_reference_t<R>, value_type>
   void push_range(R&& rg) {
-    stk_back.append_range(rg);
-    cum_back = std::ranges::fold_left(rg, cum_back, M::op);
+    for (auto&& x: std::forward<R>(rg)) {
+      const value_type& y = stk_back.emplace_back(std::forward<decltype(x)>(x));
+      cum_back = M::op(cum_back, y);
+    }
   }
   template <typename... Args>
-  decltype(auto) emplace(Args&&... args) {
-    auto x = stk_back.emplace_back(args...);
+  const value_type& emplace(Args&&... args) {
+    const value_type& x = stk_back.emplace_back(std::forward<Args>(args)...);
     cum_back = M::op(cum_back, x);
+    return x;
   }
   void pop() {
     if (stk_front.empty()) {
+      stk_front.reserve(stk_back.size());
+      cum_front.reserve(stk_back.size() + 1);
       while (!stk_back.empty()) {
         stk_front.emplace_back(std::move(stk_back.back()));
         cum_front.emplace_back(M::op(stk_front.back(), cum_front.back()));
@@ -84,15 +79,14 @@ class FoldableQueue {
 
   value_type fold() const { return M::op(cum_front.back(), cum_back); }
 
-  void swap(FoldableQueue& q) noexcept {
+  void swap(FoldableQueue& q) {
     stk_front.swap(q.stk_front);
     stk_back.swap(q.stk_back);
     cum_front.swap(q.cum_front);
-    std::swap(cum_back, q.cum_back);
+    using std::swap;
+    swap(cum_back, q.cum_back);
   }
-  friend void swap(FoldableQueue& q1, FoldableQueue& q2) noexcept {
-    q1.swap(q2);
-  }
+  friend void swap(FoldableQueue& q1, FoldableQueue& q2) { q1.swap(q2); }
 
   friend std::string pretty(const FoldableQueue& q) {
     std::string s = "[";
