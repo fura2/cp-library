a3d8c74c7da7dbf93602e8658574a1ede64cb79f
fura2 <yamazoe0577@gmail.com>
2026-07-25 16:51:56 +0900

    add MonoidImpl

--- algebra/monoid.hpp
+++ algebra/monoid.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <concepts>
+#include <string>
+#include <utility>
 
 template <typename M>
 concept Monoid = requires {
@@ -11,3 +13,35 @@ concept Monoid = requires {
 
 template <typename M>
 concept CommutativeMonoid = Monoid<M>;
+
+template <typename T, auto Op, auto Id>
+  requires requires {
+    { Id() } -> std::same_as<T>;
+  } && requires(const T& a, const T& b) {
+    { Op(a, b) } -> std::same_as<T>;
+  }
+class MonoidImpl {
+ public:
+  constexpr MonoidImpl(): x{Id()} {}
+  constexpr MonoidImpl(const T& x): x{x} {}
+  constexpr MonoidImpl(T&& x): x{std::move(x)} {}
+
+  constexpr MonoidImpl operator*(const MonoidImpl& m) const {
+    return MonoidImpl{Op(x, m.x)};
+  }
+  static constexpr MonoidImpl identity() { return MonoidImpl{Id()}; }
+
+  const T& unwrap() const { return x; }
+  T& unwrap() { return x; }
+
+  friend std::string pretty(const MonoidImpl& m)
+    requires requires(const MonoidImpl& m) {
+      { pretty(m.unwrap()) } -> std::same_as<std::string>;
+    }
+  {
+    return pretty(m.unwrap());
+  }
+
+ private:
+  T x;
+};
